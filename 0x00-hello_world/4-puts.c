71e98cdd730facb961ec372f6c46b42ad0a8e3de
cynthia402 <cynthiagw10@gmail.com>
2022-12-08 05:11:20 -0800

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-/**
- * main - Entry Point
- *
- *
+/** main - Entry Point
  * Return: Always 0 (Success)
  */
  int main (void)
-{puts("\"Programming is like building a multilingual puzzle");
+ {puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
