7ef5beef57c4816c536e1cee65881c9945340296
cynthia402 <cynthiagw10@gmail.com>
2022-12-09 07:33:33 -0800

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+if (( c == 'q' || c == 'e') !=1)
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
