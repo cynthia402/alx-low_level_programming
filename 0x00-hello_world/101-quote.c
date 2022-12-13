0b72dcc3b9843acd2ae6db416e62cc057694eb1e
cynthia402 <cynthiagw10@gmail.com>
2022-12-13 04:13:50 -0800

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 1 (Sucess)
- *     */
+ ** main - Entry point
+ **
+ ** Return: Always 1 (Sucess)
+ **/
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
