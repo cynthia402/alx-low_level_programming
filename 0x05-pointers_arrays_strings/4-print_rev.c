cb628cc1768d0731362a59a1928fdd60887c993e
cynthia402 <cynthiagw10@gmail.com>
2022-12-19 06:57:07 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * function that prints a string, in reverse, followed by a new line
+ * Return: Always success
+ */
+void print_rev(char *s);
+{
+int i = 0;
+int o;
+while (*s != '\0')
+{
+i++;
+s++;
+}
+s--;
+for (o = i; o > 0; o--)
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
