cd368686083ea8d7813874d7bd546707eb510196
cynthia402 <cynthiagw10@gmail.com>
2023-01-03 07:05:52 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memset - fill a block of memory with a specific value
+ * Return: sucess
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+for (; n > 0; i++)
+{
+s[i] = b;
+n--;
+}
+return (s);
+}
