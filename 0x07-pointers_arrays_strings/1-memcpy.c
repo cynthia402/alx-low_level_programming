3b88999b472d28ad6b7a6da3f573ecbad50ef90f
cynthia402 <cynthiagw10@gmail.com>
2023-01-03 08:28:16 -0800

    taska

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area
+ * Return: sucess
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int r = 0;
+int i = n;
+for (; r < i; r++)
+{
+dest[r] = src[r];
+n--;
+}
+return (dest);
+}
