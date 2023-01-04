aa38fc71efbb4cf79764d5a5b993752819d742f9
cynthia402 <cynthiagw10@gmail.com>
2023-01-03 23:55:01 -0800

    r

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,12 +4,14 @@
  * Return: Always 0 (Success)
  */
 char *_strpbrk(char *s, char *accept);
-int i;
+{
+int k;
 while (*s)
 {
-for (i = 0; accept[i]; i++)
+for (k = 0; accept[k]; k++)
 {
-if (*s == accept[i])
+if (*s == accept[k])
+{
 return (s);
 }
 s++;
