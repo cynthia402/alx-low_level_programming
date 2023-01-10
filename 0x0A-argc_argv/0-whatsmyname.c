c9bcbfb9cff6ac3aef1a496f9a7268c938f55f93
cynthia402 <cynthiagw10@gmail.com>
2023-01-10 00:08:16 -0800

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
