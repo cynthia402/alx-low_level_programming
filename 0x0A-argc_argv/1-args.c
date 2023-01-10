835a64facc3f5213f45366dfd23ee9faa9bcccd9
cynthia402 <cynthiagw10@gmail.com>
2023-01-10 00:33:49 -0800

    tasks

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the number of arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
