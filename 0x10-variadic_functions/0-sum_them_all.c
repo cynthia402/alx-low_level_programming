3aab957c7f0547f5cb679bada7bf7d35820f707e
cynthia402 <cynthiagw10@gmail.com>
2023-01-21 11:32:40 -0800

    l

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,14 +6,16 @@
  */
 int sum_them_all(const unsigned int n, ...
 {
-va_list valist;
-unsigned int i;
-int sum = 0;
-if (n == 0)
-return (0);
-va_start(valist, n);
-for (i = 0; i < n; i++)
-sum += va_arg(valist, int);
-va_end(valist);
-return (sum);
+va_list vls;
+unsigned int i = 0;
+va_start(vls, n);
+while (i < n)
+{
+printf("%d", va_arg(vls, int));
+i++;
+if (i < n && (separator))
+printf("%s", separator);
+}
+putchar(10);
+va_end(vls);
 }
