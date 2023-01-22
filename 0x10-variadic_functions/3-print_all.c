de708081a68e472011ef1b3de9f99e0a8131a31a
cynthia402 <cynthiagw10@gmail.com>
2023-01-22 09:52:02 -0800

    r

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,80 +1,45 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * pr_int - print a integer type;
- * @val: a va_list struct to process;
- */
-void pr_int(va_list val)
-{
-printf("%d", va_arg(val, int));
-}
-/**
- * pr_char - print a char type;
- * @val: a va_list struct to process;
- */
-void pr_char(va_list val)
-{
-printf("%c", va_arg(val, int));
-}
-/**
- * pr_float - prints a double type;
- * @val: a va_list struct to process;
- */
-void pr_float(va_list val)
-{
-printf("%f", va_arg(val, double));
-}
-/**
- * pr_str - print a string type or (nil) if empty/NULL;
- * @val: a va_list struct to process;
- */
-
-void pr_str(va_list val)
-{
-char *r;
-r = va_arg(val, char *);
-switch (!r)
-{
-case 0:
-printf("%s", r);
-break;
-case 1:
-printf("(nil)");
-break;
-}
-}
-/**
- * print_all - prints all
- * @format: param
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-int i, j;
-va_list vls;
-p_op ops[] ={
-{"c", pr_char},
-{"i", pr_int},
-{"f", pr_float},
-{"s", pr_str},
-{NULL, NULL}
-};
-va_start(vls, format);
-i = j = 0;
-while (format && format[j])
+int i = 0;
+char *str, *sep = "";
+va_list list;
+va_start(list, format);
+if (format)
 {
-i = 0;
-while (ops[i].op)
+while (format[i])
 {
-if (ops[i].op[0] == format[j])
+switch (format[i])
 {
-(ops[i].f)(vls);
-if (format[j + 1])
-printf(", ");
+case 'c':
+printf("%s%c", sep, va_arg(list, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(list, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(list, double));
+break;
+case 's':
+str = va_arg(list, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", sep, str);
+break;
+default:
+i++;
+continue;
 }
+sep = ", ";
 i++;
 }
-j++;
-putchar(10);
-va_end(vls);
+}
+printf("\n");
+va_end(list);
 }
