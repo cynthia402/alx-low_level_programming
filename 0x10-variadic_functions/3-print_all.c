c7e6fbf3bb85c9f8cf6ec961b6235bae989632a0
cynthia402 <cynthiagw10@gmail.com>
2023-01-22 09:29:55 -0800

    r

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,80 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * pr_int - print a integer type;
+ * @val: a va_list struct to process;
+ */
+void pr_int(va_list val)
+{
+printf("%d", va_arg(val, int));
+}
+/**
+ * pr_char - print a char type;
+ * @val: a va_list struct to process;
+ */
+void pr_char(va_list val)
+{
+printf("%c", va_arg(val, int));
+}
+/**
+ * pr_float - prints a double type;
+ * @val: a va_list struct to process;
+ */
+void pr_float(va_list val)
+{
+printf("%f", va_arg(val, double));
+}
+/**
+ * pr_str - print a string type or (nil) if empty/NULL;
+ * @val: a va_list struct to process;
+ */
+
+void pr_str(va_list val)
+{
+char *r;
+r = va_arg(val, char *);
+switch (!r)
+{
+case 0:
+printf("%s", r);
+break;
+case 1:
+printf("(nil)");
+break;
+}
+}
 /**
  * print_all - prints all
  * @format: param
  */
 void print_all(const char * const format, ...)
 {
-va_list valist;
-unsigned int i = 0, j, k = 0
-char *str;
-const char t_arg[] = "cifs";
-va_start(valist, format);
-while (format && format[i])
+int i, j;
+va_list vls;
+p_op ops[] ={
+{"c", pr_char},
+{"i", pr_int},
+{"f", pr_float},
+{"s", pr_str},
+{NULL, NULL}
+};
+va_start(vls, format);
+i = j = 0;
+while (format && format[j])
 {
-j = 0;
-while (t_arg[j])
+i = 0;
+while (ops[i].op)
 {
-if (format[i] == t_arg[j] && k)
+if (ops[i].op[0] == format[j])
 {
+(ops[i].f)(vls);
+if (format[j + 1])
 printf(", ");
-break;
-}
-j++;
-}
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(valist, int)), k = 1;
-break;
-case 'i':
-printf("%d", va_arg(valist, int)), k = 1;
-break; 
-case 'f':
-printf("%f", va_arg(valist, double)), k = 1;
-break; 
-case 's':
-str = va_arg(valist, char *), k = 1;
-if (!str)
-{
-printf("(nil)");
-break;
-}
-printf("%s", str);
-break;
 }
 i++;
 }
-printf("\n"), va_end(valist);
+j++;
+putchar(10);
+va_end(vls);
 }
